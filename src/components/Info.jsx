03a14bff5a6de6e9a9b71cdfc5a8cefb780c0bbe
JadeNmeister <bigdragon00@naver.com>
2024-10-12 13:31:32 +0900

    수정

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import '../styles/Info.css';
+import { Heading } from '@chakra-ui/react';
+
 
 const Info = () => {
     return (
         <div className="Info_container">
-            <h1>Info</h1>
+            <Heading as="h1" fontSize="2xl" fontWeight="bold">
+                Profile
+            </Heading>
+
         </div>
     );
 };
