cc713db2db2e90f880de1b0314a1b05bcfd4543c
JadeNmeister <bigdragon00@naver.com>
2024-11-01 15:28:13 +0900

    다크모드 반응형웹 구현 오류 해결중

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,11 +1,27 @@
+import '../styles/Projects.css'
+import { Box, Heading, SimpleGrid, VStack, Center } from "@chakra-ui/react";
+import React from "react";
 
-import '../styles/Project.css'
+const ProjectBox = ({ title, description }) => (
+    <Box className="project-box" position="relative">
+        {/* 프로젝트 박스 내용 */}
+    </Box>
+);
 
 const Projects = () => {
+    const projectsData = [
+        // 프로젝트 데이터를 여기에 추가
+    ];
+
     return (
-        <div className="Project_container">
-           프로젝트 
-        </div>
+        <VStack className="Project_container" align="stretch" spacing={4}>
+            <Center>
+                <Heading className="Project_heading" as="h2" fontSize="xl" fontWeight="bold">
+                    Project
+                </Heading>
+            </Center>
+
+        </VStack>
     );
 };
 
